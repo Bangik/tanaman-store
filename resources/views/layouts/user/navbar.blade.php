357ac64e3654bde3d829a3dca1f8839e065154bb
Achmad Dinofaldi Firmansyah <aldi.novaldi.frmnsyah@gmail.com>
2024-03-31 16:10:43 +0700

    refact: add profile nav

--- resources/views/layouts/user/navbar.blade.php
+++ resources/views/layouts/user/navbar.blade.php
@@ -24,6 +24,7 @@
                     <a href="{{route('tanaman')}}" class="nav-item nav-link {{request()->is('tanaman') ? 'active' : ''}}">Belanja Tanaman</a>
                     <a href="{{route('transactions.user.index')}}" class="nav-item nav-link {{request()->is('history-transactions') ? 'active' : ''}}">Riwayat Transaksi</a>
                     <a href="{{route('tanaman.care')}}" class="nav-item nav-link {{request()->is('tanaman-care') ? 'active' : ''}}">Tanaman Care</a>
+                    <a href="{{route('profile.index')}}" class="nav-item nav-link {{request()->is('profile') ? 'active' : ''}}">Profile</a>
                 </div>
                 <div class="d-flex m-3 me-0">
                     <a href="{{route('cart')}}" class="position-relative me-4 my-auto">
@@ -35,7 +36,7 @@
                         <span class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style="top: -5px; left: 15px; height: 20px; min-width: 20px;">0</span>
                         @endif
                     </a>
-                    <a href="{{route('profile.index')}}" class="my-auto">
+                    <a href="{{route('login')}}" class="my-auto">
                         <i class="fas fa-user fa-2x"></i>
                     </a>
                 </div>
